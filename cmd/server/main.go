799cfb325c6df6ea1d9e9400228b2f7b94830677
Tobias B <germansuperioz@yahoo.de>
2021-03-29 20:30:50 +0200

    Add basic postgres connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"github.com/upper/db/v4/adapter/postgresql"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/cownetwork/indigo/proto"
 )
@@ -12,6 +14,24 @@ import (
 func main() {
 	log.Println("Hello World!")
 
+	connUrl := &postgresql.ConnectionURL{
+		Host:     getEnvOrDefault("POSTGRES_URL", "localhost:5432"),
+		User:     getEnvOrDefault("POSTGRES_USER", "test"),
+		Password: getEnvOrDefault("POSTGRES_PASSWORD", "password"),
+		Database: getEnvOrDefault("POSTGRES_DB", "test_database"),
+	}
+
+	log.Printf("Connecting to PostgresSQL at %s ...", connUrl.Host)
+
+	sess, err := postgresql.Open(connUrl)
+	if err != nil {
+		log.Fatalf("failed to connect to postgres: %v", err)
+	}
+	defer sess.Close()
+
+	log.Println("Connected to PostgresSQL.")
+
+	// setup grpc server
 	lis, err := net.Listen("tcp", ":6969")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -23,6 +43,14 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func getEnvOrDefault(env string, def string) string {
+	value := os.Getenv(env)
+	if len(value) == 0 {
+		return def
+	}
+	return value
 }
 
 type rolesServiceServer struct {
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/cownetwork/indigo
 go 1.16
 
 require (
+	github.com/lib/pq v1.10.0 // indirect
+	github.com/upper/db/v4 v4.1.0
 	google.golang.org/grpc v1.36.1
 	google.golang.org/protobuf v1.26.0
 )
